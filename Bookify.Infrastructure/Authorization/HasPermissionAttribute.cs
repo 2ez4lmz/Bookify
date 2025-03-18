8233965ab0419bd0a57556cfb74f5b4993f57826
Diamond <valiullinoff@gmail.com>
2025-03-18 22:25:13 +0500

    ImplementPermissionBasedAuthorization

---
+++ Bookify.Infrastructure/Authorization/HasPermissionAttribute.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace Bookify.Infrastructure.Authorization;
+
+public class HasPermissionAttribute : AuthorizeAttribute
+{
+    public HasPermissionAttribute(string permission)
+        : base(permission)
+    {
+    }
+}
