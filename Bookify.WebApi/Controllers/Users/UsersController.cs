7906fe743f61df82eaa121eefc2102c2f3f15517
Diamond <valiullinoff@gmail.com>
2025-03-18 18:52:10 +0500

    AddGetLoggedInUserEndpoint

--- Bookify.WebApi/Controllers/Users/UsersController.cs
+++ Bookify.WebApi/Controllers/Users/UsersController.cs
@@ -1,5 +1,7 @@
+using Bookify.Application.Users.GetLoggedInUser;
 using Bookify.Application.Users.LogInUser;
 using Bookify.Application.Users.RegisterUser;
+using Bookify.Domain.Abstractions;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,16 @@ namespace Bookify.WebApi.Controllers.Users
         public UsersController(ISender sender)
         {
             _sender = sender;
+        }
+        
+        [HttpGet("me")]
+        public async Task<IActionResult> GetLoggedInUser(CancellationToken cancellationToken)
+        {
+            var query = new GetLoggedInUserQuery();
+
+            Result<UserResponse> result = await _sender.Send(query, cancellationToken);
+
+            return Ok(result.Value);
         }
 
         [AllowAnonymous]
